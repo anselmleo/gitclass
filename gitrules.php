7f212fafd00d5d5ee4afe1292858fab57fb54c2b
Leo anselm <anselmleo@gmail.com>
2019-03-06 12:04:45 +0100

    Create gitrules.php

---
+++ gitrules.php
@@ -0,0 +1,26 @@
+<?php
+
+Echo "10 Most Important Rules For Git Source Control Management 
+		
+		1. Always check git status before every activity
+
+		2. Always have one repository per project if possible
+
+		3. Add a thoroughgly meaningful message per commit - will save lifes
+
+		4. Commit regularly and messages should be in present tense - like you about to do it 
+
+		5. Ofcourse create a branch for your work - don't work directly on the master branch
+
+		6. Avoid merge commits - they give little to no information on commits made - use rebase instead.
+
+		7. Use 'git fetch' opposed to 'git pull' because of the - merge commits that follow
+
+		8. Use rebase to replay HEAD and sync timeline appropriately
+
+		9. Use merge to place HEAD back in place
+
+		10. Always make sure tree is clean before carrying out any branching operation
+
+	 ";
+
--- readme.md
+++
@@ -1,9 +0,0 @@
-TODO
-====
-1. fix bugs on master
-2. clear all db tables
-
-NEW TODO
-========
-1. add authentication feature
-
